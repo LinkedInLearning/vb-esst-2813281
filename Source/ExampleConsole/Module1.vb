38a7bb43c40b23b08568f013a9f5e9a1cf8393b4
Walt Ritscher <waltritscher@outlook.com>
2019-08-09 22:43:50 -0700

    Final code

--- Source/ExampleConsole/Module1.vb
+++ Source/ExampleConsole/Module1.vb
@@ -9,10 +9,14 @@
 			If counter > 30 Then
 				counter = 2
 			End If
+			If Console.KeyAvailable Then
+				currentChar = Console.ReadKey.KeyChar
+			End If
+
 			outputString = New String(currentChar, counter)
 			Console.WriteLine(outputString)
 			Threading.Thread.Sleep(120)
-		Loop While True
+		Loop While currentChar <> " "c
 
 	End Sub
 End Module
