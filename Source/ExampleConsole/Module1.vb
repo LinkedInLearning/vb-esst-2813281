3e8dc7f1293130a1b5eec61799c3bf68dc61f136
Walt Ritscher <waltritscher@outlook.com>
2019-08-09 18:34:04 -0700

    Add started code and comments

--- Source/ExampleConsole/Module1.vb
+++ Source/ExampleConsole/Module1.vb
@@ -1,7 +1,11 @@
 ﻿Module Module1
 
 	Sub Main()
+		' do something here
 
+		' Wait for user entry, 
+		' a crude way to stop the program ending before we can see the results
+		Console.ReadLine()
 	End Sub
 
 End Module
