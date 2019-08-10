6302b2df87ad856d4c0622efb4d6884162c050c4
Walt Ritscher <waltritscher@outlook.com>
2019-08-09 22:28:20 -0700

    Add starting code

--- Source/ExampleConsole/Module1.vb
+++ Source/ExampleConsole/Module1.vb
@@ -1,25 +1,16 @@
 ﻿Module Module1
 
 	Sub Main()
-
-		Dim counter As Integer = 2
-		Dim outputString As String
-		Dim currentChar As Char = "-"c
+		Dim counter = 2
+		Dim outputString
+		Dim currentChar = "-"c
 		Do
 			counter = counter + 1
-			If counter > 80 Then
-				counter = 2
-			End If
-			If Console.KeyAvailable Then
-				currentChar = Console.ReadKey.KeyChar
-			End If
-
-
 			outputString = New String(currentChar, counter)
 			Console.WriteLine(outputString)
+			Threading.Thread.Sleep(120)
+		Loop While True
 
-			Threading.Thread.Sleep(100)
-		Loop While currentChar <> " "c
 	End Sub
 
 End Module
