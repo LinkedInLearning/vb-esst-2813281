d641d913cb345f6c293315da39b66b61dc9b4fef
Walt Ritscher <waltritscher@outlook.com>
2019-08-09 21:41:53 -0700

    Stub in end code

--- Source/ExampleConsole/Module1.vb
+++ Source/ExampleConsole/Module1.vb
@@ -1,11 +1,12 @@
 ﻿Module Module1
 
 	Sub Main()
-		' do something here
 
-		' Wait for user entry, 
-		' a crude way to stop the program ending before we can see the results
-		Console.ReadLine()
+		Do
+
+
+		Loop While True
+
 	End Sub
 
 End Module
