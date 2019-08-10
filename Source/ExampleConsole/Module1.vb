32de0187a88c132a0d7546b12ce3561bd639734a
Walt Ritscher <waltritscher@outlook.com>
2019-08-09 21:38:25 -0700

    End versions

--- Source/ExampleConsole/Module1.vb
+++ Source/ExampleConsole/Module1.vb
@@ -1,11 +1,24 @@
 ﻿Module Module1
 
 	Sub Main()
-		' do something here
+		Dim counter = 2
+		Dim outputString As String
+		Dim currentChar = "-"c
+		Do
+			counter = counter + 1
+			If counter > 80 Then
+				counter = 2
+			End If
+			If Console.KeyAvailable Then
+				currentChar = Console.ReadKey.KeyChar
+			End If
 
-		' Wait for user entry, 
-		' a crude way to stop the program ending before we can see the results
-		Console.ReadLine()
+
+			outputString = New String(currentChar, counter)
+			Console.WriteLine(outputString)
+
+			Threading.Thread.Sleep(100)
+		Loop While currentChar <> " "c
 	End Sub
 
 End Module
