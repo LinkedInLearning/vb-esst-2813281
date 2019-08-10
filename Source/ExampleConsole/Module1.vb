873dbabcc7fefc804c71c911873e6f235cd47b27
Walt Ritscher <waltritscher@outlook.com>
2019-08-09 21:52:08 -0700

    Final version

--- Source/ExampleConsole/Module1.vb
+++ Source/ExampleConsole/Module1.vb
@@ -1,10 +1,14 @@
 ﻿Module Module1
 
 	Sub Main()
-
+		Dim counter = 2
+		Dim outputString
+		Dim currentChar = "-"c
 		Do
-
-
+			counter = counter + 1
+			outputString = New String(currentChar, counter)
+			Console.WriteLine(outputString)
+			Threading.Thread.Sleep(120)
 		Loop While True
 
 	End Sub
