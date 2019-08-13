0c7b0e3baaa991ad8264b167c55af5690792f658
Walt Ritscher <waltritscher@outlook.com>
2019-08-13 10:17:23 -0700

    Create Code runner windows app

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -1,3 +1,14 @@
 ﻿Class MainWindow
+	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles GoButton.Click
+		' Add code here
+		Dim counter As Integer = 22
+		ShowResults(counter)
+	End Sub
+	Sub ShowResults(message As string)
+		MessageTextBox.Text = message + vbCrLf
+	End Sub
 
+	Private Sub Clear(sender As Object, e As RoutedEventArgs) Handles ClearButton.Click
+			MessageTextBox.Text = ""
+	End Sub
 End Class
