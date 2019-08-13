89b0c9ede0db06d826ac0924eebbe157bac50c29
Walt Ritscher <waltritscher@outlook.com>
2019-08-13 16:53:41 -0700

    Rename the ShowMessage method

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 
 Class MainWindow
-	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles ConstantButton.Click
+	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles RunCodeButton.Click
 		' constants are usually declared at the module or class level
 
 		Const Pi As Decimal = 3.1415926535897931
@@ -10,11 +10,11 @@ Class MainWindow
 		Dim circumference As Double
 		Dim diameter As Double = 4
 		circumference = Pi * diameter
-		ShowResults(circumference)
-		ShowResults(HALF_BYTE)
+		OutputToScreen(circumference)
+		OutputToScreen(HALF_BYTE)
 	End Sub
 
-	Private Sub BuiltInButton_Click(sender As Object, e As RoutedEventArgs) Handles BuiltInButton.Click
+	Private Sub BuiltInButton_Click(sender As Object, e As RoutedEventArgs) Handles RunCode2Button.Click
 
 		' Constants are members of some .NET classes.
 
@@ -26,15 +26,15 @@ Class MainWindow
 
 		Dim message1, message2 As String
 		message1 = "Hello" + vbCrLf + "again."
-		ShowResults(message1)
+		OutputToScreen(message1)
 		message2 = "which type of conversion to perform when calling the StrConv function."
 
 		message2 = StrConv(message2, VbStrConv.ProperCase)
-		ShowResults(message2)
+		OutputToScreen(message2)
 	End Sub
 
 
-	Sub ShowResults(message As String)
+	Sub OutputToScreen(message As String)
 		MessageTextBox.Text += message + vbCrLf
 	End Sub
 
