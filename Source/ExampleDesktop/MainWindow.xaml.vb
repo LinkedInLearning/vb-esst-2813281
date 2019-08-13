167d7a8a9aadbf995bdb6cd5f83255225228141e
Walt Ritscher <waltritscher@outlook.com>
2019-08-13 16:36:43 -0700

    Rename button controls

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -1,36 +1,33 @@
 ﻿Imports System.Runtime.CompilerServices
 
 Class MainWindow
-	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles ConstantButton.Click
-		' constants are usually declared at the module or class level
+	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles RunCodeButton.Click
+		' variable have scope and lifetime
+		' scope determines its visibility to other code
 
-		Const Pi As Decimal = 3.1415926535897931
-		Const HALF_BYTE As Byte = 127
+		Dim counter As Integer = 1
 
-		Dim circumference As Double
-		Dim diameter As Double = 4
-		circumference = Pi * diameter
-		ShowResults(circumference)
-		ShowResults(HALF_BYTE)
+		' counter is local variable, can access within this method
+
+		If counter > 10 Then
+			Dim message As String = "block scope"
+			' message is block scope, only visible in this if block
+			ShowResults(message)
+
+		End If
+		If counter > 20 Then
+			Dim message As String = "block scope"
+			' message is block scope, only visible in this if block
+			ShowResults(message)
+		End If
+		' what about this variable?
+		'	message = "Cannot access this variable"
+
 	End Sub
 
-	Private Sub BuiltInButton_Click(sender As Object, e As RoutedEventArgs) Handles BuiltInButton.Click
+	Private Sub RunCode2(sender As Object, e As RoutedEventArgs) Handles RunCode2Button.Click
 
-		' Constants are members of some .NET classes.
 
-		Dim circumference As Double
-		Dim diameter As Double = 4
-		circumference = Math.PI * diameter
-
-		' VB language has some constants that start with vb_
-
-		Dim message1, message2 As String
-		message1 = "Hello" + vbCrLf + "again."
-		ShowResults(message1)
-		message2 = "which type of conversion to perform when calling the StrConv function."
-
-		message2 = StrConv(message2, VbStrConv.ProperCase)
-		ShowResults(message2)
 	End Sub
 
 
