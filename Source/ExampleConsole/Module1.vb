56e0a1f8452aba5d9f6d2139516072d9cee3bc7d
Staff Author <waltritscher@outlook.com>
2019-08-13 18:43:33 -0700

    Add console property

--- Source/ExampleConsole/Module1.vb
+++ Source/ExampleConsole/Module1.vb
@@ -7,9 +7,10 @@ Module Module1
 		Dim folderName As String = "C:\VbTemp"
 		Dim files As String()
 		files = Directory.GetFiles(folderName)
-
+		Console.ForegroundColor = ConsoleColor.DarkRed
 		For Each file In files
 			Console.WriteLine(file)
+
 		Next
 
 		Console.ReadLine()
