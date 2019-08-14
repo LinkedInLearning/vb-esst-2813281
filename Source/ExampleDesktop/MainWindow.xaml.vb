e8263e2e33517452a463a82c9265ece35b278d25
Walt Ritscher <waltritscher@outlook.com>
2019-08-13 22:21:44 -0700

    Add explainer code

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -1,50 +1,37 @@
 ﻿Imports System.Runtime.CompilerServices
 
 Class MainWindow
-	' class level scope, variable is visible to all code in class
-	' in OOP this is also considered a Field (a member of the class type)
-
-	Private primaryBrush As New SolidColorBrush
-	Private _accentBrush As New SolidColorBrush ' naming convention preferred by some OOP programmers
 
 
 	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles RunCodeButton.Click
-		' variable have scope and lifetime
-		' scope determines its visibility to other code
 
-		Dim counter As Integer = 15
 
-		' counter is local variable, can access within this method
+		Dim title As String = "Code explorer is a WPF application."
+		OutputToScreen(title)
 
-		If counter > 10 Then
-			Dim message As String = "block scope"
-			' message is block scope, only visible in this if block
-			OutputToScreen(message)
+		Dim thisMessage As String = "I'll use for showing code examples. "
+		OutputToScreen(thisMessage)
 
-		End If
-		If counter > 20 Then
-			Dim message As String = "block scope"
-			' message is block scope, only visible in this if block
-			OutputToScreen(message)
-		End If
-		' what about this variable?
-		'	message = "Cannot access this variable"
 
 	End Sub
 
 	Private Sub RunCode2(sender As Object, e As RoutedEventArgs) Handles RunCode2Button.Click
-		primaryBrush.Color = Colors.Orange
-		MainPanel.Background = primaryBrush
+
+		Dim counter As Integer = 15
+
+		OutputToScreen(counter)
+		OutputLine()
+		counter += 12
+		OutputToScreen(counter)
 
 	End Sub
-	Private Sub RunCode3Button_Click(sender As Object, e As RoutedEventArgs) Handles RunCode3Button.Click
-		_accentBrush.Color = Colors.Purple
-		MessageTextBox.Foreground = primaryBrush
-		MessageTextBox.Background = _accentBrush
-	End Sub
+
 
 	Sub OutputToScreen(message As String)
 		MessageTextBox.Text += message + vbCrLf
+	End Sub
+	Sub OutputLine()
+		MessageTextBox.Text += "»-»-»-»-»-»-»-»-»-»-»-»" + vbCrLf
 	End Sub
 
 	Private Sub Clear(sender As Object, e As RoutedEventArgs) Handles ClearButton.Click
