7a6ce821e891f636b59e807b6d150059936866d3
Staff Author <waltritscher@outlook.com>
2019-08-15 16:41:13 -0700

    Recorded successfully

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -5,60 +5,63 @@ Class MainWindow
 	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles RunCodeButton.Click
 		Dim myInteger As Integer = 43218765
 		Dim myLong As Long = 9988776655
-		
 
 		OutputToScreen("--No format--")
+		' code here
 		OutputToScreen(myInteger)
 		OutputToScreen(myLong)
-		OutputBlankLine()
 
+		OutputBlankLine()
 		OutputToScreen("--Number------")
+		' code here
 		OutputToScreen(myInteger.ToString("N"))
 		OutputToScreen(myLong.ToString("N"))
 		OutputBlankLine()
-
 		OutputToScreen("--Exponent-------")
+		' code here
 		OutputToScreen(myInteger.ToString("E"))
 		OutputToScreen(myLong.ToString("E"))
 		OutputBlankLine()
-
 		OutputToScreen("--Currency--------")
+		' code here
 		OutputToScreen(myInteger.ToString("C"))
 		OutputToScreen(myLong.ToString("C"))
-		OutputBlankLine
-		Dim myByte As Byte = 127
-	
+		OutputBlankLine()
 		OutputToScreen("--Hexadecimal--------")
-		OutputToScreen(myByte)
+		' code here
+		Dim myByte As Byte = 127
+		OutputToScreen(myByte.ToString())
 		OutputToScreen(myByte.ToString("X"))
-		
-
+#Region "Learn More"
 		' learn more about format strings
 		'	https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
+#End Region
 	End Sub
 
 	Private Sub RunCode2(sender As Object, e As RoutedEventArgs) Handles RunCode2Button.Click
-		
-		Dim myDate As Date = #10/21/2025#
+
+
 		OutputToScreen("--Date--------")
+		' code here
+		Dim myDate As Date = #10/21/2025#
 		OutputToScreen(myDate)
 		OutputToScreen(myDate.ToString("D"))
 
+
+#Region "Learn More"
 		' learn more about data format strings
 		' https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+#End Region
 
 	End Sub
 
-	Private Sub RunCode3Button_Click(sender As Object, e As RoutedEventArgs) Handles RunCode3Button.Click
 
-
-	End Sub
 
 	Sub OutputToScreen(message As String)
 		MessageTextBox.Text += message + vbCrLf
 	End Sub
 	Sub OutputBlankLine()
-		MessageTextBox.Text +=  vbCrLf
+		MessageTextBox.Text += vbCrLf
 	End Sub
 
 	Sub OutputFormattedToScreen(message As String, number As Object)
