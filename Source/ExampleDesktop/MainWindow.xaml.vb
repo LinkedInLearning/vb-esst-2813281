db687ee2ebfee565dd4525653ed91958f6ad3801
Walt Ritscher <waltritscher@outlook.com>
2019-08-14 22:27:19 -0700

    Clean up code

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -1,26 +1,13 @@
-﻿Imports System.Runtime.CompilerServices
-
-Class MainWindow
-	' class level scope, variable is visible to all code in class
-	' in OOP this is also considered a Field (a member of the class type)
-
-	Private primaryBrush As New SolidColorBrush
-
-	' underscore naming convention preferred by some OOP programmers
-	Private _accentBrush As New SolidColorBrush
-
-
+﻿Class MainWindow
 
 	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles RunCodeButton.Click
-
-
 
 	End Sub
 
 	Private Sub RunCode2(sender As Object, e As RoutedEventArgs) Handles RunCode2Button.Click
 
-
 	End Sub
+
 	Private Sub RunCode3Button_Click(sender As Object, e As RoutedEventArgs) Handles RunCode3Button.Click
 
 	End Sub
@@ -32,6 +19,5 @@ Class MainWindow
 	Private Sub Clear(sender As Object, e As RoutedEventArgs) Handles ClearButton.Click
 		MessageTextBox.Text = ""
 	End Sub
-
 
 End Class
