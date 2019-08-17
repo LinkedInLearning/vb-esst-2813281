b9126645b6456d8617987192470ee67f9423a843
Staff Author <waltritscher@outlook.com>
2019-08-16 23:23:36 -0700

    Finished version

--- Source/ExampleDesktop/NumberGenerator.vb
+++ Source/ExampleDesktop/NumberGenerator.vb
@@ -1,22 +1,30 @@
 ﻿Module NumberGenerator
 
 	Public Function GetRandomOddNumber() As Integer
-		Dim ran = New Random
-		Dim candidate As Integer = ran.Next(maxValue:=201)
-		If candidate Mod 2 = 1 Then
-			Return candidate
-		Else
-			Return candidate + 1
-		End If
+		Dim candidate As Integer = GetRandomNumber(201)
+		Return GetOddOrEven(candidate, True)
 
 	End Function
 	Public Function GetRandomEvenNumber() As Integer
-		Dim ran = New Random
-		Dim candidate As Integer = ran.Next(maxValue:=201)
-		If candidate Mod 2 = 0 Then
+		Dim candidate As Integer = GetRandomNumber(201)
+		Return GetOddOrEven(candidate, False)
+	End Function
+
+	Private Function GetOddOrEven(candidate As Integer, wantOdd As Boolean) As Integer
+		Dim remainder As Integer = 0
+		If wantOdd Then
+			remainder = 1
+		End If
+		If candidate Mod 2 = remainder Then
 			Return candidate
 		Else
 			Return candidate + 1
 		End If
+	End Function
+
+	Private Function GetRandomNumber(maxValue As Integer) As Integer
+		Dim ran = New Random
+		Dim candidate As Integer = ran.Next(maxValue:=maxValue)
+		Return candidate
 	End Function
 End Module
