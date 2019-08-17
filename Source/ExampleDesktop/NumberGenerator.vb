a4ea30991a95960d90584c9d0ed524706622e2ae
Staff Author <waltritscher@outlook.com>
2019-08-16 19:08:32 -0700

    Final Code

--- Source/ExampleDesktop/NumberGenerator.vb
+++ Source/ExampleDesktop/NumberGenerator.vb
@@ -1,3 +1,11 @@
 ﻿Module NumberGenerator
+	Public Sub PrintDocument()
 
+	End Sub
+	Public Function GetRandomOddNumber() As Integer
+		Return 0
+	End Function
+	Public Function GetRandomEvenNumber() As Integer
+		Return 0
+	End Function
 End Module
