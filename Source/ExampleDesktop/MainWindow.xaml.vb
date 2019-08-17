ba4b918bd4e56501f7726c601a95a5ca5a4ad7bd
Staff Author <waltritscher@outlook.com>
2019-08-17 02:14:33 -0700

    Rename solution folder

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -4,8 +4,10 @@ Class MainWindow
 
 	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
 		' Challenge: Part 1
-		' allow the user to enter the max value
-		' by entering an integer in the NumberTextBox 
+		' allow the user to choose the max value
+		' by entering an integer in the NumberTextBox. 
+		' Don't forget to validate the input 
+		' from NumberTextBox to prevent non integer values.
 
 		Dim odd = GetRandomOddNumber()
 
@@ -14,8 +16,10 @@ Class MainWindow
 
 	Private Sub RunCode2(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
 		' Challenge: Part 1
-		' allow the user to enter the max value
-		' by entering an integer in the NumberTextBox 
+		' allow the user to choose the max value
+		' by entering an integer in the NumberTextBox.
+		' Don't forget to validate the input 
+		' from NumberTextBox to prevent non integer values.
 		Dim even = GetRandomEvenNumber()
 		OutputToScreen(even)
 
@@ -24,6 +28,7 @@ Class MainWindow
 	Private Sub RunCode3(sender As Object, e As RoutedEventArgs) Handles ButtonC.Click
 		' Challenge: Part 2
 		' show a random squared value
+		' when this button is clicked
 		' when this button is clicked
 		' 2, 4, 9, 16, 25, 36 etc.
 		' reuse or refactor the code in Module NumberGenerator
