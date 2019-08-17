3ca604f7c1944a3a37a8b74a8ef11335941f6d2b
Walt Ritscher <waltritscher@outlook.com>
2019-08-17 11:13:06 -0700

    Add start code

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -3,12 +3,22 @@
 Class MainWindow
 
 	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
-
+		Dim computerName As String
+		computerName = "Webserver14"
+		Dim outputText = "The current computer name is " + computerName
+		OutputToScreen(outputText)
 
 	End Sub
 
 	Private Sub RunCode2(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
-
+		Dim x As Double
+		x = 6
+		' breakpoints cannot be set on comments
+		' Or other non-runnable lines
+		Dim y As Double = 7
+		Dim result As String
+		Dim fraction As Double = x / 5
+		result = $"Your answer is {fraction}"
 
 	End Sub
 
