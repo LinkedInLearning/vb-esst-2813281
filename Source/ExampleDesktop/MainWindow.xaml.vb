4180784e04a217f6cc2e0607758e4e009b2e31de
Walt Ritscher <waltritscher@outlook.com>
2019-08-19 12:42:12 -0700

    Update UI, add images, remove console project add demo code

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -3,118 +3,89 @@ Imports System.Globalization
 
 Class MainWindow
 
-	Private Sub CreateString_Click(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
-		Dim direction = "East"
-		Dim otherDirection As String = "West"
-		' use the constructor to build a string
-		Dim line = New String("»"c, 63)
-		OutputToScreen(direction)
-		OutputToScreen(line)
+	Private Sub IfDemo(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
+		Dim odd = GetRandomOddNumber(401)
+		CurrentNumberTextBlock.Text = odd
+		If odd < 100 Then
+			Message1TextBox.Text += odd.ToString() + vbCrLf
+		End If
+	End Sub
+
+	Private Sub IfElseDemo(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
+		Dim odd = GetRandomOddNumber(401)
+		CurrentNumberTextBlock.Text = odd
+		If odd < 100 Then
+			Message1TextBox.Text += odd.ToString() + vbCrLf
+		Else
+			Message3TextBox.Text += odd.ToString() + vbCrLf
+		End If
 
 	End Sub
 
-	Private Sub Concatenate_Click(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
-		Dim teamA = TeamATextBox.Text
-		Dim teamB = TeamBTextBox.Text
-		Dim message = "Don't miss this match between top-ranked (" + teamA + ") and long-time rivals (" & teamB & ")."
-		OutputToScreen(message)
+	Private Sub IfElseIfDemo(sender As Object, e As RoutedEventArgs) Handles ButtonC.Click
 
-		' string.format
-		message = String.Format("Meet the teams: ({0}) and long-time rivals ({1}).", teamA, teamB)
-		OutputToScreen(message)
-		' Interpolated strings 
-		message = $"Autographs and Selfies with: ({teamA}) and long-time rivals ({teamB})."
-		OutputToScreen(message)
+		Dim odd = GetRandomOddNumber(401)
+		CurrentNumberTextBlock.Text = odd
+		If odd < 100 Then
+			Message1TextBox.Text += odd.ToString() + vbCrLf
+			ElseIf	odd > 300 Then
+			Message2TextBox.Text += odd.ToString() + vbCrLf
+		Else
+			Message3TextBox.Text += odd.ToString() + vbCrLf
+		End If
+
+	End Sub
+	Private Sub AndDemo(sender As Object, e As RoutedEventArgs) Handles ButtonE.Click
+
+		If DayTextBox.Text = "Monday" And TimeTextBox.Text = "3:00" Then
+			PizzaImage.Visibility = Visibility.Visible
+			CouponImage.Visibility = Visibility.Hidden
+		Else
+			PizzaImage.Visibility = Visibility.Hidden
+			CouponImage.Visibility = Visibility.Visible
+		End If
 
 	End Sub
 
-	Private Sub Search_Click(sender As Object, e As RoutedEventArgs) Handles ButtonD.Click
-		Dim message = "Personalize your LinkedIn Learning experience with skills you need."
-		OutputToScreen(message)
-		' verify if a sub string is available
-
-		Dim periodExists = message.EndsWith(".")
-		Dim wordExists = message.StartsWith("Hello")
-
-		' finding by index
-		' return -1 if not found, otherwise the index location
-		Dim searchWord = SearchWordTextbox.Text
-		OutputToScreen($"Use Contains(), '{searchWord}' Exists: {message.Contains(searchWord)}")
-		Dim wordIndex As Integer = message.IndexOf(searchWord)
-		OutputToScreen($"Use IndexOf,Word Index for '{searchWord}': {wordIndex}")
-
-
-	End Sub
-	Private Sub Modify_Click(sender As Object, e As RoutedEventArgs) Handles ButtonE.Click
-
-		Dim lettersWithSpaces = "  AA  "
-		Dim trimmed = lettersWithSpaces.Trim(" ")
-
-		'  trim these chars
-		Dim charsToTrim = New Char() {","c, "."c, " "c, "!"c}
-		Dim message = "A synonym for cinnamon, is a cinnamon synonym. Say cinnamon swiftly and soundlessly!!!"
-
-		trimmed = message.TrimEnd(charsToTrim)
-		OutputToScreen(message)
-		OutputToScreen(trimmed)
-		OutputLine()
-
-		Dim upper = message.ToUpper()
-		Dim lower = message.ToLower()
-		OutputToScreen(upper)
-		OutputToScreen(lower)
-		OutputLine()
-
-		Dim replaced = message.Replace("cinnamon", "cardamom")
-		OutputToScreen(message)
-		OutputToScreen(replaced)
-
-	End Sub
-
-	Private Sub Split_click(sender As Object, e As RoutedEventArgs) Handles ButtonF.Click
-		Dim message = "Personalize your LinkedIn Learning experience with skills you need."
-		Dim words = message.Split(" "c)
-
-		For Each word In words
-			OutputToScreen(word)
-		Next
-		OutputLine()
-		Dim newSentence = String.Join("~", words)
-		OutputToScreen(newSentence)
-
+	Private Sub OrDemo(sender As Object, e As RoutedEventArgs) Handles ButtonG.Click
+		If DayTextBox.Text = "Monday" Or TimeTextBox.Text = "3:00" Then
+			PizzaImage.Visibility = Visibility.Visible
+			CouponImage.Visibility = Visibility.Hidden
+		Else
+			PizzaImage.Visibility = Visibility.Hidden
+			CouponImage.Visibility = Visibility.Visible
+		End If
 	End Sub
 
 
-
-	Sub OutputToScreen(message As String)
-		MessageTextBox.Text += message + vbCrLf
-	End Sub
-	Sub OutputBlankLine()
-		MessageTextBox.Text += vbCrLf
-	End Sub
-
-
-	Sub OutputLine()
-		MessageTextBox.Text += "-----------" + vbCrLf
-	End Sub
 
 	Private Sub Clear(sender As Object, e As RoutedEventArgs) Handles ClearButton.Click
-		Clear()
+			Clear()
 	End Sub
 
-	Private Sub Clear()
-		MessageTextBox.Text = ""
+	Private sub Clear()
+		Message1TextBox.Text = ""
+			Message2TextBox.Text = ""
+			Message3TextBox.Text = ""
+	End sub
+
+	Private Async Sub AndSlow(sender As Object, e As RoutedEventArgs) Handles ButtonH.Click
+		SlowMessageTextBlock.Text = "processing..."
+		If IsNetWorkAvailable() And Await IsDatabaseOnline() Then
+			SlowMessageTextBlock.Text = "And: Can access database."
+		Else
+			SlowMessageTextBlock.Text = "And: Cannot access database."
+		End If
 	End Sub
 
-	'Private Sub Chars_Click(sender As Object, e As RoutedEventArgs) Handles ButtonC.Click
-	'	' use indexer to get a char from string
-	'	Dim message = "Now is a good time to learn .NET."
-	'	Dim ninthChar = message(9)
-	'	OutputToScreen(message)
-	'	OutputToScreen(ninthChar)
-	'End Sub
+	Private Async Sub AndAlsoSlow(sender As Object, e As RoutedEventArgs) Handles ButtonJ.Click
+		SlowMessageTextBlock.Text = "processing..."
+		If IsNetWorkAvailable() AndAlso Await IsDatabaseOnline() Then
+			SlowMessageTextBlock.Text = "AndAlso: Can access database."
+		Else
+			SlowMessageTextBlock.Text = "AndAlso: Cannot access database."
+		End If
+	End Sub
 
-	' use IndexOfAny to search for multiple candidates
-	' search stops as soon as first match is found
-	' Dim invalidCharsFound As Integer = message.IndexOfAny(New Char() {"!"c, "@"c, "#"c, "$"c})
+
 End Class
---
+++ Source/ExampleDesktop/SlowFunctions.vb
@@ -0,0 +1,12 @@
+﻿Module SlowFunctions
+
+	Public  Function IsNetWorkAvailable () As  Boolean
+		Return True
+	End Function
+
+	Public async Function IsDatabaseOnline () As Task (Of Boolean)
+	await	Task.Delay(3000)
+
+		Return True
+	End Function
+End Module
