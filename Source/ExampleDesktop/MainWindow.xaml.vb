ac4f7bf563a80813119009b1023e756776354191
Staff Author <waltritscher@outlook.com>
2019-08-19 15:56:23 -0700

    Cleanup code

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -38,33 +38,6 @@ Class MainWindow
 		End If
 
 	End Sub
-	Private Sub AndDemo(sender As Object, e As RoutedEventArgs) Handles ButtonE.Click
-
-		If DayTextBox.Text = "Monday" And TimeTextBox.Text = "3:00" Then
-			PizzaImage.Visibility = Visibility.Visible
-			CouponImage.Visibility = Visibility.Hidden
-			PizzaAwardTextBox.Text += "Free Pizza!" + vbCrLf
-		Else
-			PizzaImage.Visibility = Visibility.Hidden
-			CouponImage.Visibility = Visibility.Visible
-			PizzaAwardTextBox.Text += "No Pizza, Here's a coupon." + vbCrLf
-		End If
-
-	End Sub
-
-	Private Sub OrDemo(sender As Object, e As RoutedEventArgs) Handles ButtonG.Click
-		If DayTextBox.Text = "Monday" Or TimeTextBox.Text = "3:00" Then
-			PizzaImage.Visibility = Visibility.Visible
-			CouponImage.Visibility = Visibility.Hidden
-			PizzaAwardTextBox.Text += "Free Pizza!" + vbCrLf
-		Else
-			PizzaImage.Visibility = Visibility.Hidden
-			CouponImage.Visibility = Visibility.Visible
-			PizzaAwardTextBox.Text += "No Pizza, Here's a coupon." + vbCrLf
-		End If
-	End Sub
-
-
 
 	Private Sub Clear(sender As Object, e As RoutedEventArgs) Handles ClearButton.Click
 		Clear()
@@ -74,25 +47,11 @@ Class MainWindow
 		LowNumberOutput.Text = ""
 		HighNumberOutput.Text = ""
 		OtherNumberOutput.Text = ""
-	End sub
-
-	Private Async Sub AndSlow(sender As Object, e As RoutedEventArgs) Handles ButtonH.Click
-		SlowMessageTextBlock.Text = "processing..."
-		If IsNetWorkAvailable() And Await IsDatabaseOnline() Then
-			SlowMessageTextBlock.Text = "And: Can access database."
-		Else
-			SlowMessageTextBlock.Text = "And: Cannot access database."
-		End If
 	End Sub
 
-	Private Async Sub AndAlsoSlow(sender As Object, e As RoutedEventArgs) Handles ButtonJ.Click
-		SlowMessageTextBlock.Text = "processing..."
-		If IsNetWorkAvailable() AndAlso Await IsDatabaseOnline() Then
-			SlowMessageTextBlock.Text = "AndAlso: Can access database."
-		Else
-			SlowMessageTextBlock.Text = "AndAlso: Cannot access database."
-		End If
-	End Sub
+
+
+
 
 
 End Class
