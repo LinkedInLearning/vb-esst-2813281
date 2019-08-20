dc4dee0af7c1d50339df08a1adaaeb65f78fa8d8
Staff Author <waltritscher@outlook.com>
2019-08-19 18:13:53 -0700

    Update UI with better AndAlso text

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -7,18 +7,18 @@ Class MainWindow
 	Private Async Sub AndSlow(sender As Object, e As RoutedEventArgs) Handles ButtonH.Click
 		SlowMessageTextBlock.Text = "processing..."
 		If IsNetWorkAvailable() And Await IsDatabaseOnline() Then
-			SlowMessageTextBlock.Text = "And: Can access database."
+			SlowMessageTextBlock.Text = "And: The Network and DB are available."
 		Else
-			SlowMessageTextBlock.Text = "And: Cannot access database."
+			SlowMessageTextBlock.Text = "And: Network off-line."
 		End If
 	End Sub
 
 	Private Async Sub AndAlsoSlow(sender As Object, e As RoutedEventArgs) Handles ButtonJ.Click
 		SlowMessageTextBlock.Text = "processing..."
 		If IsNetWorkAvailable() AndAlso Await IsDatabaseOnline() Then
-			SlowMessageTextBlock.Text = "AndAlso: Can access database."
+			SlowMessageTextBlock.Text = "AndAlso: The Network and DB are available."
 		Else
-			SlowMessageTextBlock.Text = "AndAlso: Cannot access database."
+			SlowMessageTextBlock.Text = "AndAlso: Network off-line."
 		End If
 	End Sub
 
