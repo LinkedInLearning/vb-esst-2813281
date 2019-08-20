42af9e534fe07aa1f9c211b6db4c4538b97f326f
Staff Author <waltritscher@outlook.com>
2019-08-19 22:16:00 -0700

    Save start state

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -2,27 +2,11 @@
 
 Class MainWindow
 
-	Private Sub IfDemo(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
+	Private Sub SelectCaseDemo(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
 		Dim currentNumber = GetNextNumber()
+		CurrentNumberOutput.Text = currentNumber
 
-		CurrentRandomOutput.Text = currentNumber
-		' game rules
-		' 1-10 = $50.00
-		' 90-100 =
-		Select Case currentNumber
-			Case 1 To 3
-				Text1.Text &= currentNumber & vbCrLf
-			Case 5, 7, 9
-				Text2.Text &= currentNumber & vbCrLf
-			Case 4, 6, 8
-				Text3.Text &= currentNumber & vbCrLf
-			Case 10 To 15, 17 To 19
-				Text4.Text &= currentNumber & vbCrLf
-			Case Is < 30
-				Text5.Text &= currentNumber & vbCrLf
-			Case Else
-				text6.Text &= currentNumber & vbCrLf
-		End Select
+
 
 	End Sub
 
