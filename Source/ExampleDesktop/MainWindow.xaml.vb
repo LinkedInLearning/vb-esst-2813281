89b7c7843c1195742e34d7d7c23ee1ec0cbfda3d
Staff Author <waltritscher@outlook.com>
2019-08-20 23:46:33 -0700

    Successfully recorded

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -3,8 +3,17 @@
 Class MainWindow
 
 	Private Async Sub NewDemo(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
-	
-	
+
+
+		Dim colorNamesArray(20) As String
+		Dim oddNumbersArray(10) As Integer
+
+		Dim colorNamesList As New List(Of String)
+		Dim oddNumbersList = New List(Of Integer)
+
+		colorNamesArray(3) = "Blue"
+
+		ReDim colorNamesArray(30)
 
 	End Sub
 
