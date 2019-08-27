0967c14a6a12fce53660121b25274595a1db7246
Staff Author <waltritscher@outlook.com>
2019-08-26 21:55:59 -0700

    Add enumerable demo code

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -8,55 +8,26 @@ Class MainWindow
 	Private Sub EnumerableDemo(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
 
 		' LINQ provides tools for querying lists of data 
-		' The Enumerable class is key to query actions.
+		' The Enumerable class is the primary LINQ class for query actions.
 
 
 		' get a range of integers
 
 		Dim numbers = Enumerable.Range(100, 300).ToList()
 
-		' use Enumerable extension methods to query the numbers list
-
-		Dim total = numbers.Sum
-		OriginalList.Items.Add(total)
-
-	End Sub
-
-
-	Private Sub WhereDemo(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
-
-		' where
-		Dim integerList = Enumerable.Range(1, 300).ToList()
-
-		Dim listOf25s = integerList.Where(Function(x) x Mod 25 = 0 Or x < 10)
-
-		OriginalList.ItemsSource = integerList
-		ModifiedList.ItemsSource = listOf25s
 
 
 	End Sub
 
-	Private Sub OrderByDemo(sender As Object, e As RoutedEventArgs) Handles ButtonC.Click
+
+	Private Sub ExtensionMethodsDemo(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
 
 
-		Dim words = New List(Of String)({"aa-5-aa", "bb-1-bb", "rr-6-rr", "zz-3-zz"})
-		Dim orderedWords = words.OrderBy(Of String)(Function(x) x.Chars(3))
-
-
-		OriginalList.ItemsSource = words
-		ModifiedList.ItemsSource = orderedWords
-
-	End Sub
-
-	Private Sub FindDemo(sender As Object, e As RoutedEventArgs) Handles ButtonD.Click
+		Dim compassPoints As New List(Of String)({"North", "East", "South", "West"})
 		Dim doubleList = New List(Of Double)({3.3, 4.4, 1.1, 2.2, 5.5, 8.8, 9.9, 6.6, 7.7})
-		Dim lastNumber = doubleList.Last()
 
-		Dim maxValue = doubleList.Max
-
-		OriginalList.ItemsSource = doubleList
-		ModifiedList.Items.Add($"last number in list: {lastNumber}")
-		ModifiedList.Items.Add($"Highest number in list: {maxValue}")
+		' use the Enumerable extension methods with any class
+		' that implements the IEnumerable or IEnumerable (Of T) interface
 
 	End Sub
 
