45e2b32a97dc40a69c5ab9dca6dd67bf16b8cfd8
Staff Author <waltritscher@outlook.com>
2019-08-27 15:18:01 -0700

    Recorded Successfully

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -8,9 +8,9 @@ Class MainWindow
 
 		' use Enumerable extension methods to query the numbers list
 
-		Dim total As Integer
-		Dim average As Double
-		Dim subsetAverage As Double
+		Dim total As Integer = numbers.Sum
+		Dim average As Double = numbers.Average
+		Dim subsetAverage As Double = numbers.Take(4).Average
 
 		OriginalList.Items.Add("Integers : 2, 4, 5, 6, 8, 20, 35, 50, 65, 89")
 		OriginalList.Items.Add($"Total: {total}")
@@ -25,10 +25,10 @@ Class MainWindow
 		' where
 		Dim integerList = Enumerable.Range(1, 300).ToList()
 
-
+		Dim listOf25s = integerList.Where(Function(x) x Mod 25 = 0 Or x < 10)
 
 		OriginalList.ItemsSource = integerList
-		' ModifiedList.ItemsSource = listOf25s
+		ModifiedList.ItemsSource = listOf25s
 
 
 	End Sub
@@ -37,21 +37,23 @@ Class MainWindow
 
 
 		Dim words = New List(Of String)({"aa-5-aa", "bb-1-bb", "rr-6-rr", "zz-3-zz"})
-
+		Dim orderedWords = words.OrderBy(Of String)(Function(x) x.Chars(3))
 
 
 		OriginalList.ItemsSource = words
-		'	ModifiedList.ItemsSource = orderedWords
+		ModifiedList.ItemsSource = orderedWords
 
 	End Sub
 
 	Private Sub FindDemo(sender As Object, e As RoutedEventArgs) Handles ButtonD.Click
 		Dim doubleList = New List(Of Double)({3.3, 4.4, 1.1, 2.2, 5.5, 8.8, 9.9, 6.6, 7.7})
 
+		Dim maxValue = doubleList.Max
+		Dim lastNumber = doubleList.Last
 
 		OriginalList.ItemsSource = doubleList
-		'ModifiedList.Items.Add($"Last number in list: {lastNumber}")
-		'ModifiedList.Items.Add($"Highest number in list: {maxValue}")
+		ModifiedList.Items.Add($"Last number in list: {lastNumber}")
+		ModifiedList.Items.Add($"Highest number in list: {maxValue}")
 
 	End Sub
 
