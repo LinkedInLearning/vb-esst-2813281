9f8ee0c0610bd640c217731071539abf40f05784
Staff Author <waltritscher@outlook.com>
2019-08-27 17:17:42 -0700

    Remove Marble Generator code

--- Source/ExampleDesktop/MarbleGenerator.vb
+++ Source/ExampleDesktop/MarbleGenerator.vb
@@ -2,13 +2,6 @@
 
 
 
-	Public Function GetMarbles(marbleCount As Integer, marbleName As String) As List(Of String)
-		Dim temp = New List(Of String)
-		For index = 1 To marbleCount
-			temp.Add(marbleName)
 
-		Next
-		Return temp
-	End Function
 
 End Module
