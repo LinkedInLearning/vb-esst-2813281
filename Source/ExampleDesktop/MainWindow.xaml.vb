15475fe0df441e76cac15f42d8f4a3060b13029a
Staff Author <waltritscher@outlook.com>
2019-08-29 16:37:36 -0700

    Final prep for demo

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -2,35 +2,8 @@
 
 Class MainWindow
 	Dim _counter As Integer = 0
-	Private Sub Catch1Demo(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
-
-		Try
-			_counter += 1
-			FileATextBox.Text = _counter
-			FileBTextBox.Text = ErrorMaker.OpenFile("c:\nofile.txt")
-
-		Catch
-
-		End Try
 
 
-
-
-
-	End Sub
-
-	Private Sub Catch2Demo(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
-		Try
-			_counter += 1
-			FileATextBox.Text = _counter
-
-			FileBTextBox.Text = ErrorMaker.OpenFile("c:\nofile.txt")
-		Catch ex As Exception
-			MessageBox.Show($"Something went wrong { ex.Message}")
-		End Try
-
-
-	End Sub
 
 	Private Sub Catch3Demo(sender As Object, e As RoutedEventArgs) Handles ButtonC.Click
 		Try
@@ -45,12 +18,10 @@ Class MainWindow
 		Catch ex As OverflowException
 			MessageBox.Show($"This looks like a division error")
 		Catch ex As Exception
-			
-			' this block if for unforeseen exceptions
-			' log the exception
-		'	My.Log.WriteException (ex)
-			My.Log.WriteException (ex,TraceEventType.Critical, "Add a custom log message/")
-				MessageBox.Show($"Something went wrong { ex.Message}")
+
+			' this block is for unexpected exceptions
+			' best log the exception
+			MessageBox.Show($"Something went wrong { ex.Message}")
 		End Try
 
 
