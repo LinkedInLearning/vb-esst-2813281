aee8f9d9c0ea5ae9a781c57c636c9acb69f342ee
Walt Ritscher <waltritscher@outlook.com>
2019-09-01 10:15:38 -0700

    Completed challenge

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -6,80 +6,26 @@ Class MainWindow
 	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
 
 
-		' current date-time
-
-
-		' more readable with named parameters
-
-
-
-		'OutputToScreen($"literalDate: {literalDate}")
-		'OutputToScreen($"parsedDate: {parsedDate}")
-		'OutputToScreen($"fromDateClass: {fromDateClass}")
-
-		'OutputLine()
-		'OutputToScreen($"currentDate: {currentDate}")
-		'OutputLine()
-
-		'OutputToScreen($"readableDate: {readableDate}")
-	End Sub
-	Private Sub RunCode2(sender As Object, e As RoutedEventArgs) Handles ButtonD.Click
-		Dim literalTime As Date = #5:06:10 PM#
-		' more readable with named parameters
-		Dim fromDateClass = New Date(year:=2007, month:=2, day:=27, hour:=16, minute:=30, second:=55)
-
-		OutputToScreen($"literalTime: {literalTime}")
-		OutputToScreen($"fromDateClass: {fromDateClass}")
-		OutputToScreen($"fromDateClass Formatted: {fromDateClass.ToLongTimeString}")
-
-		OutputBlankLine()
-	End Sub
-
-	Private Sub RunCode3(sender As Object, e As RoutedEventArgs) Handles ButtonB.Click
-
-		Dim startDate, endDate As Date
+		Dim startDate As Date
+		Dim interval As integer
 		If Date.TryParse(StartDateTextBox.Text, startDate) = False Then
 			OutputToScreen("Cannot parse the start date.")
 			Exit Sub
 		End If
-		If Date.TryParse(EndDateTextBox.Text, endDate) = False Then
+		If Integer.TryParse(IntervalTextBox.Text, interval) = False Then
 
-			OutputToScreen("Cannot parse the end date.")
+			OutputToScreen("Cannot parse the interval.")
 			Exit Sub
 		End If
 
-		OutputToScreen(startDate.ToLongDateString)
-		OutputToScreen(endDate.ToShortDateString)
-
-		OutputToScreen("custom date format: " + startDate.ToString("yyyy--MM--dd"))
-		OutputToScreen($"EndDate, Day: {endDate.Day}")
-		OutputToScreen($"EndDate, DayOfWeek: {endDate.DayOfWeek}")
-		OutputToScreen($"EndDate, DayOfYear: {endDate.DayOfYear}")
-		OutputToScreen($"EndDate, IsDaylightSaving: {endDate.IsDaylightSavingTime}")
-
-	End Sub
-
-	Private Sub RunCode4(sender As Object, e As RoutedEventArgs) Handles ButtonC.Click
-		' how many days between days
-		Dim startDate, endDate As Date
-		If Date.TryParse(StartDateTextBox.Text, startDate) = False Then
-			OutputToScreen("Cannot parse the start date.")
-			Exit Sub
-		End If
-		If Date.TryParse(EndDateTextBox.Text, endDate) = False Then
-
-			OutputToScreen("Cannot parse the end date.")
-			Exit Sub
-		End If
-
-		Dim myTimeSpan As TimeSpan ' the distance between dates
+		
 		OutputToScreen("Original Date: " + startDate.ToLongDateString)
-		Dim newDate = startDate.AddDays(5)
-		OutputToScreen("Add Days: " + newDate.ToLongDateString)
-		myTimeSpan = endDate.Subtract(startDate)
-
-		OutputToScreen("Days between: " + myTimeSpan.TotalDays.ToString)
+		OutputToScreen("Number of days and Months to add: " + interval.ToString)
+		Dim newDate = startDate.AddDays(interval).AddMonths(interval)
+		OutputToScreen("Calculated End date: " + newDate.ToLongDateString)
 	End Sub
+
+
 
 	Sub OutputToScreen(message As String)
 		MessageTextBox.Text += message + vbCrLf
