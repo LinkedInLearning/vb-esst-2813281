2bc62b31fc08f80dc05df6fe501144dbe7ced038
Walt Ritscher <waltritscher@outlook.com>
2019-09-01 12:45:26 -0700

    Create starter project

--- Source/ExampleDesktop/MainWindow.xaml.vb
+++ Source/ExampleDesktop/MainWindow.xaml.vb
@@ -5,32 +5,7 @@ Class MainWindow
 
 	Private Sub RunCode(sender As Object, e As RoutedEventArgs) Handles ButtonA.Click
 
-		AnswerTextBox.Text = ""
-		Dim searchWords = SearchWordTextBox.Text.ToLower
-
-		Dim results As String = ""
-		Dim found1, found2, foundAll As Boolean
-		found1 = String1Textbox.Text.ToLower.Contains(searchWords)
-		found2 = String2Textbox.Text.ToLower.Contains(searchWords)
-		foundAll = found1 And found2
-		If found1 Then
-			results &= $"(+)  '{searchWords}' found in first string {vbCrLf}"
-		Else
-			results &= $"(-)  '{searchWords}' not found in first string {vbCrLf}"
-		End If
-
-		If found2 Then
-			results &= $"(+)  '{searchWords}' found in second string {vbCrLf}"
-			Else
-			results &= $"(-)  '{searchWords}' not found in second string {vbCrLf}"
-		End If
-
-		If foundAll Then
-			results &= $"(++)'{searchWords}' found in both strings {vbCrLf}"
-		End If
-
-
-		AnswerTextBox.Text = results
+	
 
 
 	End Sub
